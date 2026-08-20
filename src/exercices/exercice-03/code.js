08a748f03462f021ec5f4b77f213cdaf1b4a065d
Pablo Hernandez Soto <pablohernandezsoto.28@outlook.com>
2026-08-20 15:01:05 +0200

    exercie-03

---
+++ src/exercices/exercice-03/code.js
@@ -0,0 +1,37 @@
+import { html, LitElement } from "lit";
+
+class Exercice03 extends LitElement {
+  static get props() {
+    return {
+      name: { type: String },
+      id: { type: String },
+    };
+  }
+
+  constructor() {
+    super();
+    this.name = "Pablo Hernández Soto";
+    this.id = "div-id";
+  }
+
+  render() {
+    return html`
+      <h1>Props en litelement por enlaces</h1>
+
+      <!-- Bind por enlace de texto -->
+      <p>Nombre: ${this.name}</p>
+
+      <!-- Bind por enlace de atributo -->
+      <div id="${this.id}">Enlace de atributo</div>
+
+      <!-- Bind por enlace de evento -->
+      <button @click="${this.clickHandler}">Click me</button>
+    `;
+  }
+
+  clickHandler() {
+    console.log("Has hecho click");
+  }
+}
+
+customElements.define("exercice-03", Exercice03);
