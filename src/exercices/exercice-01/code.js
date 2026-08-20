37929461b6869e7d79e00dc40265438d8aee49ae
Pablo Hernandez Soto <pablohernandezsoto.28@outlook.com>
2026-08-20 14:21:22 +0200

    start basic lit project

---
+++ src/exercices/exercice-01/code.js
@@ -0,0 +1,9 @@
+import { html, LitElement } from "lit";
+
+class Exercice01 extends LitElement {
+  render() {
+    return html` <h1>Exercice 01</h1> `;
+  }
+}
+
+customElements.define("exercice-01", Exercice01);
