ff349716ea80db306eb883128c7cebc77021c2b0
Pablo Hernandez Soto <pablohernandezsoto.28@outlook.com>
2026-08-20 15:18:48 +0200

    exercie-05

---
+++ src/exercices/exercice-05/code.js
@@ -0,0 +1,46 @@
+import { html, LitElement } from "lit";
+
+class Exercice05 extends LitElement {
+  static get props() {
+    return {
+      article: { type: Object },
+      prop1: { type: String },
+      prop2: { type: String },
+    };
+  }
+
+  constructor() {
+    super();
+
+    this.article = {
+      title: "Blog en litelement",
+      description: "description en litelement",
+      content: "content en litelement",
+    };
+
+    this.prop1 = "Propiedad 1";
+    this.prop2 = "Propiedad 2";
+  }
+
+  render() {
+    return html`
+      <h1>Plantillas en litelement</h1>
+
+      ${this.showTextOne()}
+      <hr />
+      ${this.showTextTwo()}
+    `;
+  }
+
+  showTextOne() {
+    return html`<h2>${this.article.title}</h2>
+      <p>${this.article.description}</p>
+      <p>${this.article.content}</p>`;
+  }
+
+  showTextTwo() {
+    return html` <span>${this.prop1} ${this.prop2}</span> `;
+  }
+}
+
+customElements.define("exercice-05", Exercice05);
